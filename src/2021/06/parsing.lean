38ae00bee4f10396ab36745939d4e3bf961acf93
Gihan Marasingha <gihan.marasingha@gmail.com>
2021-06-20 12:04:17 +0100

    applications of the many parser

--- src/2021/06/parsing.lean
+++ src/2021/06/parsing.lean
@@ -3,7 +3,8 @@ namespace parser
 
 /-
 This is a translation into Lean of the `parsing` module described in the first edition of
-"Programming in Haskell", by Graham Hutton, Chapter 8.
+"Programming in Haskell", by Graham Hutton, Chapter 8:
+http://www.cs.nott.ac.uk/~pszgmh/Parsing.lhs
 -/
 
 inductive Parser (α : Type)
@@ -94,6 +95,8 @@ def letter : Parser char := sat (λ c, to_bool c.is_alpha)
 
 def alphanum : Parser char := sat (λ c, to_bool c.is_alphanum)
 
+def is_space : Parser char := sat (λ c, to_bool c.is_whitespace)
+
 def charp (x : char) : Parser char := sat (λ c, c = x)
 
 example : parse digit "123" = some ('1', "23") := rfl
@@ -150,13 +153,121 @@ example : parse (n_or_less lower 600000) "gihanIIHAN" = none := rfl
 example : parse (n_or_less lower 4) "gihanIIHAN" = some (['g', 'i', 'h', 'a'], "nIIHAN") := rfl     
 
 /-
-`many p` does `p` repeatedly until failure. It's a `meta def` as it uses unbounded recursion.
+`many p` does `p` repeatedly until failure. It always succeeds.
+It's a `meta def` as it uses unbounded recursion.
 The Lean parser module gets around this by using a natural number to store the position in the string.
+
+Note: Graham Hutton used mutual recursion to define `many` and `many1`. I didn't choose this
+approach as Lean 3 has limited support for mutual recursion.
 -/
 meta def many (p : Parser α) : Parser (list α) :=
   (do x ← p, xs ← many, return (x :: xs)) <|> return []
 
 #eval parse (many digit) "754asdf"
+
+#eval parse (many digit) "abc" -- succeeds and returns the empty list.
+
+/-
+`many1 p` is like `many` except that it will fail if `p` doesn't succeed at least once.
+-/
+meta def many1 (p : Parser α) : Parser (list α) :=
+  (do x ← p, xs ← many p, return (x :: xs)) <|> failure
+
+#eval parse (many1 digit) "754asdf"
+
+#eval parse (many1 digit) "abc" -- fails
+
+meta def identc : Parser (list char) :=
+  do  x ← lower,
+      xs ← many alphanum,
+      return (x :: xs)
+
+meta def ident : Parser string := do xs ← identc, return ⟨xs⟩
+
+#eval parse ident "abc343[]!!" -- returns "abc343"
+
+#eval parse ident "121abc343[]!!" -- fails
+
+/-
+`char_to_nat c` converts the character `c` to a corresponding `ℕ`.
+As Lean functions are total, we choose to convert `c` to `0` if `c` is not a digit.
+-/
+def char_to_nat : char → ℕ
+  | '0' := 0
+  | '1' := 1
+  | '2' := 2
+  | '3' := 3
+  | '4' := 4
+  | '5' := 5
+  | '6' := 6
+  | '7' := 7
+  | '8' := 8
+  | '9' := 9
+  | _ := 0
+
+-- Converts a list of digits (in 'big-endian' order) into a natural number.
+def list_nat_to_nat : list ℕ → ℕ
+  | [] := 0
+  | (x :: xs) := x + 10 * list_nat_to_nat xs
+
+example : list_nat_to_nat [1,9,4] = 491 := rfl
+
+def list_char_to_nat (xs : list char) : ℕ :=
+  list_nat_to_nat (list.map char_to_nat (list.reverse xs))
+
+example : list_char_to_nat ['1','2','3'] = 123 := rfl
+
+-- Recall non-digit chars are converted to `0`:
+example : list_char_to_nat ['1','p', '3'] = 103 := rfl
+
+meta def natp : Parser ℕ :=
+  do  xs ← many1 digit,
+      return (list_char_to_nat xs)
+
+#eval parse natp "4563bob" -- gets `4563` as a natural number
+
+meta def intp : Parser ℤ :=
+  (do  charp '-',
+      n ← natp,
+      return (-n))
+  <|> do n ← natp, return n
+
+#eval parse intp "132bob"
+
+#eval parse intp "-123bob"
+
+meta def space : Parser unit := do many is_space, return ()
+
+#eval parse space " ▸  ¬ fish" -- consumes the whitespace, returning `star`.
+
+meta def token (p : Parser α) : Parser α :=
+  do  space,
+      v ← p,
+      space,
+      return v
+
+#eval parse (token natp) "    123" -- returns the natural number `123`
+
+meta def identifier : Parser string := token ident
+
+#eval parse identifier "   gill   sdfd"
+
+meta def natural : Parser ℕ := token natp
+
+#eval parse natural "   1232"
+
+meta def symbol (xs : string) : Parser string := token (stringp xs)
+
+#eval parse (symbol "hema") "   hema"
+
+meta def test_parser1 : Parser (list ℕ) := 
+  do  symbol "[",
+      n ← natural,
+      ns ← many (do symbol ",", natural),
+      symbol "]",
+      return (n :: ns)
+
+#eval parse test_parser1 "  [1,2,  3 ] rubbish" -- returns `[1, 2, 3] : list ℕ`.
 
 end parser
 end exlean
